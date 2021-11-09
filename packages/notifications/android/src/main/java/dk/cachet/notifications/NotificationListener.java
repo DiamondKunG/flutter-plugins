e37ccf5df9dbcb5648bfe944ccde16e3a770d01a
EditCraft Dev <f_ff4@hotmail.com>
2021-11-09 19:49:57 +0700

    fix fucking null

--- packages/notifications/android/src/main/java/dk/cachet/notifications/NotificationListener.java
+++ packages/notifications/android/src/main/java/dk/cachet/notifications/NotificationListener.java
@@ -23,7 +23,7 @@ public class NotificationListener extends NotificationListenerService {
 
   @RequiresApi(api = VERSION_CODES.KITKAT)
   @Override
-  public void onNotificationPosted(@NonNull StatusBarNotification notification) {
+  public void onNotificationPosted(StatusBarNotification notification) {
     // Retrieve package name to set as title.
     String packageName = notification.getPackageName();
     // Retrieve extra object from notification to extract payload.
